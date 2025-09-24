b11781f23742d6a206b88d5e444648a1278abb82
JulianaStadler <julianastadlerberger@gmail.com>
2025-09-24 11:29:09 -0300

    add cat api

--- Aula13-Consumo-de-dados-externos-GET/src/App.jsx
+++ Aula13-Consumo-de-dados-externos-GET/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cat, setCat] = useState({});
+
+  useEffect(() => {
+    getCat();
+  }, []);
+
+  const getCat = async () => {
+    const response = await axios.get('https://cataas.com/cat/gif');
+    console.log(response.data)
+    setCat(response.data)
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type="button" onClick={() => {getCat(); alert("changing cat, await")}} style={{position: 'absolute', top: "10px", transform: "translate(-50%, 0)", background: "green"}}>New Cat</button> <br /> <br />
+      <img src={cat.url} alt="opa" srcset="" style={{maxWidth: "100%"}} />
     </>
   )
 }
